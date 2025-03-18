5b7ef300557aa540aa0c3563da5d60860987524f
mrq <mrq@mrq.cz>
2025-03-19 00:44:13 +0100

    proper ownership transfer

--- tasks/roles/00_gho-transfer-ownership.ts
+++ tasks/roles/00_gho-transfer-ownership.ts
@@ -1,29 +1,39 @@
-import { expect } from 'chai';
 import { GhoToken } from './../../../types/src/contracts/gho/GhoToken';
 import { task } from 'hardhat/config';
 
 task('gho-transfer-ownership', 'Transfer Ownership of Gho')
   .addParam('newOwner')
   .setAction(async ({ newOwner }, hre) => {
+    const { deployer } = await hre.getNamedAccounts();
     const DEFAULT_ADMIN_ROLE = hre.ethers.utils.hexZeroPad('0x00', 32);
-
     const gho = (await hre.ethers.getContract('GhoToken')) as GhoToken;
-    if (false) {
-      console.log(newOwner, 'is already admin');
-    } else {
-      const tx1 = await gho.grantRole(gho.FACILITATOR_MANAGER_ROLE(), newOwner);
-      await tx1.wait();
-      const tx2 = await gho.grantRole(gho.BUCKET_MANAGER_ROLE(), newOwner);
-      await tx2.wait();
-      const grantAdminRoleTx = await gho.grantRole(DEFAULT_ADMIN_ROLE, newOwner);
-      await grantAdminRoleTx.wait();
-      // await expect(grantAdminRoleTx).to.emit(gho, 'RoleGranted');
+    const roles = [
+      DEFAULT_ADMIN_ROLE,
+      await gho.FACILITATOR_MANAGER_ROLE(),
+      await gho.BUCKET_MANAGER_ROLE(),
+    ];
 
-      const { deployer } = await hre.getNamedAccounts();
-      const removeAdminRoleTx = await gho.renounceRole(DEFAULT_ADMIN_ROLE, deployer);
-      await removeAdminRoleTx.wait();
-      // TODO
-
-      console.log(`GHO ownership transferred to:  ${newOwner}`);
+    for (const role of roles) {
+      const hasRole = await gho.hasRole(role, newOwner);
+      if (!hasRole) {
+        const tx = await gho.grantRole(role, newOwner);
+        await tx.wait();
+        console.log(`Role ${role} granted to ${newOwner}`);
+      } else {
+        console.log(`Role ${role} already granted to ${newOwner}`);
+      }
     }
+
+    for (const role of roles.reverse()) {
+      const hasRole = await gho.hasRole(role, deployer);
+      if (hasRole) {
+        const tx = await gho.renounceRole(role, deployer);
+        await tx.wait();
+        console.log(`Role ${role} renounced from ${deployer}`);
+      } else {
+        console.log(`Role ${role} already renounced from ${deployer}`);
+      }
+    }
+
+    console.log(`GHO ownership transferred to:  ${newOwner}`);
   });
