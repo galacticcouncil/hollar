656d212b2f40dcb7513d9a5da0e0306c782a8e08
mrq <mrq@mrq.cz>
2025-03-19 15:54:16 +0100

    added constants to the zero discount rate strategy to support ui helper

--- src/contracts/facilitators/aave/interestStrategy/ZeroDiscountRateStrategy.sol
+++ src/contracts/facilitators/aave/interestStrategy/ZeroDiscountRateStrategy.sol
@@ -9,11 +9,16 @@ import {IGhoDiscountRateStrategy} from '../interestStrategy/interfaces/IGhoDisco
  * @notice Discount Rate Strategy that always return zero discount rate.
  */
 contract ZeroDiscountRateStrategy is IGhoDiscountRateStrategy {
+  uint256 public constant GHO_DISCOUNTED_PER_DISCOUNT_TOKEN = 0;
+  uint256 public constant DISCOUNT_RATE = 0;
+  uint256 public constant MIN_DISCOUNT_TOKEN_BALANCE = 0;
+  uint256 public constant MIN_DEBT_TOKEN_BALANCE = 0;
+
   /// @inheritdoc IGhoDiscountRateStrategy
   function calculateDiscountRate(
     uint256 debtBalance,
     uint256 discountTokenBalance
   ) external view override returns (uint256) {
-    return 0;
+    return DISCOUNT_RATE;
   }
 }
