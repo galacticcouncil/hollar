e63ea50fa6827d3c027bd2f5ab4fa315490c7347
mrq <mrq@mrq.cz>
2025-10-22 20:33:56 +0200

    5M HOLLAR

--- helpers/config.ts
+++ helpers/config.ts
@@ -15,7 +15,7 @@ export const ghoReserveConfig = {
 export const ghoEntityConfig = {
   label: 'Hydration Market',
   entityAddress: ZERO_ADDRESS,
-  mintLimit: ethers.utils.parseUnits('4.0', 24), // 4M
+  mintLimit: ethers.utils.parseUnits('5.0', 24), // 5M
   flashMinterLabel: 'HOLLAR FlashMinter',
   flashMinterCapacity: ethers.utils.parseUnits('0.1', 24), // 100k
   flashMinterMaxFee: ethers.utils.parseUnits('10000', 0), // 100%
